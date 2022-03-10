d4574307c4b0c722cc60c9fc303df5f730e90d7f
Kei Nishimura <kei@okashimi.com>
2022-03-10 20:42:33 +0900

    Remove button click effect

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,31 +4,37 @@ import { MantineProvider, MantineThemeOverride } from '@mantine/core'
 import '@/styles/main.css'
 
 const getTheme = (pathname: string): MantineThemeOverride => {
+  const theme = {}
+
   if (pathname.startsWith('/blue')) {
     return {
+      ...theme,
       primaryColor: 'blue'
     }
   }
 
   if (pathname.startsWith('/green')) {
     return {
+      ...theme,
       primaryColor: 'green'
     }
   }
 
   if (pathname.startsWith('/red')) {
     return {
+      ...theme,
       primaryColor: 'red'
     }
   }
 
   if (pathname.startsWith('/dark')) {
     return {
+      ...theme,
       colorScheme: 'dark'
     }
   }
 
-  return {}
+  return theme
 }
 
 export default function App ({ Component, pageProps, router }: AppProps) {
@@ -42,6 +48,15 @@ export default function App ({ Component, pageProps, router }: AppProps) {
         withNormalizeCSS
         withGlobalStyles
         theme={theme}
+        styles={{
+          Button: () => ({
+            root: {
+              '&:not(:disabled):active': {
+                transform: 'none'
+              }
+            }
+          })
+        }}
       >
         <Component {...pageProps} />
       </MantineProvider>
